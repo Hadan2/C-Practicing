b1d905e38a8924256d5546c5f01af3ef5ddb8f5f
Hadan2 <zinzzaboy00@naver.com>
2022-09-08 13:23:24 +0900

    12

--- Project1/Project1/소스2.cpp
+++ Project1/Project1/소스2.cpp
@@ -1,3 +1,16 @@
+#include <iostream>
+using namespace std;
+
 int main() {
+	/* x++와 ++x의 차이점 */
+	int a, b;  
+	a = 10;
+	b = 4;
+	a = b;
+	b = 7;
+	cout << "a:";
+	cout << a;
+	cout << "b:";
+	cout << b;
 
 }
